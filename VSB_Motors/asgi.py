cdc27c3956cbef452435321864dfac015a9aeedd
Alessandro <verlanti2002@gmail.com>
2022-03-09 21:56:04 +0100

    Add backend django project

---
+++ VSB_Motors/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for VSB_Motors project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VSB_Motors.settings')
+
+application = get_asgi_application()
