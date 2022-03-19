66a8df1a3eaca37e2db50598252423636870cf03
Alessandro <verlanti2002@gmail.com>
2022-03-19 20:48:13 +0100

    add homepage, shop, base

--- VSB_Motors/settings.py
+++ VSB_Motors/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0dnp!^@cf%7fg0w$6ka9=d&lm084uh-*c)8tbz!c3l6$9c@&r^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,8 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- vsb_app/views.py
+++ vsb_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class HomepageView(TemplateView):
+    template_name = "vsb_app/index.html"
+
+
+class ShopView(TemplateView):
+    template_name = "vsb_app/shop.html"
+
+
+# class ShopView(TemplateView):
+#     template_name = "vsb_app/my_transaction.html"
+#
+#
+# class ShopView(TemplateView):
+#     template_name = "vsb_app/sell_my_car.html"
+#
+#
+# class ShopView(TemplateView):
+#     template_name = "vsb_app/sign_in.html"
+#
+#
+# class ShopView(TemplateView):
+#     template_name = "vsb_app/sign_up.html"
+#
+#
+# class ShopView(TemplateView):
+#     template_name = "vsb_app/single_product.html"
