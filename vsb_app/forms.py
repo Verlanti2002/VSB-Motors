7fc331bae4a04d578ba85ced4f4328e235ff72ac
Alessandro <verlanti2002@gmail.com>
2022-03-28 22:30:01 +0200

    modify sell your car and single product

--- vsb_app/forms.py
+++ vsb_app/forms.py
@@ -5,26 +5,25 @@ from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from .models import Automobile, ImmaginiAutomobili, Marca, current_year
 
 
-
 def year_choices():
     return [(r, r) for r in range(1900, datetime.date.today().year+1)]
 
 
 class AutomobileForm(forms.ModelForm):
 
-    targa = forms.CharField(widget=forms.TextInput(attrs={'class': 'input_box'}))
-    marca = forms.ModelChoiceField(queryset=Marca.objects.all(), widget=forms.Select(attrs={'class': 'input_box'}), empty_label="Seleziona")
-    modello = forms.CharField(widget=forms.TextInput(attrs={'class': 'input_box'}))
-    carburante = forms.ChoiceField(choices=Automobile.FUEL_CHOICES, widget=forms.Select(attrs={'class': 'input_box'}))
-    cilindrata = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'input_box'}))
-    cavalli = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'input_box'}))
-    potenza = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'input_box'}))
-    stato = forms.ChoiceField(choices=Automobile.STATE_CHOICES, widget=forms.Select(attrs={'class': 'input_box'}))
-    km_percorsi = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'input_box'}))
-    anno_produzione = forms.TypedChoiceField(coerce=int, choices=year_choices, initial=current_year, widget=forms.Select(attrs={'class': 'input_box'}))
-    numero_proprietari = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'input_box'}))
-    prezzo = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'input_box'}))
-    cambio = forms.ChoiceField(choices=Automobile.CHANGE_CHOICES, widget=forms.Select(attrs={'class': 'input_box'}))
+    targa = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Inserire la targa'}))
+    marca = forms.ModelChoiceField(queryset=Marca.objects.all(), widget=forms.Select, empty_label="Seleziona la marca")
+    modello = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Inserire il modello'}))
+    carburante = forms.ChoiceField(choices=Automobile.FUEL_CHOICES, widget=forms.Select)
+    cilindrata = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Inserire la cilindrata'}))
+    cavalli = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Inserire i cavalli'}))
+    potenza = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Inserire la potenza'}))
+    stato = forms.ChoiceField(choices=Automobile.STATE_CHOICES, widget=forms.Select)
+    km_percorsi = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Inserire i kilometri percorsi'}))
+    anno_produzione = forms.TypedChoiceField(coerce=int, choices=year_choices, initial=current_year, widget=forms.Select)
+    numero_proprietari = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Inserire il numero di proprietari'}))
+    prezzo = forms.DecimalField(widget=forms.NumberInput(attrs={'placeholder': 'Inserire il prezzo'}))
+    cambio = forms.ChoiceField(choices=Automobile.CHANGE_CHOICES, widget=forms.Select)
 
     class Meta:
         model = Automobile
